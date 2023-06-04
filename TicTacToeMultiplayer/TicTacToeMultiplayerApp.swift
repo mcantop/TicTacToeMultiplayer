ae63e8ff5b95ccf09e095da8da9b0e7bb6958369
mcantop <maciejcantop@icloud.com>
2023-06-04 05:21:58 +0200

    Initial Commit

---
+++ TicTacToeMultiplayer/TicTacToeMultiplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeMultiplayerApp.swift
+//  TicTacToeMultiplayer
+//
+//  Created by Maciej on 04/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeMultiplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
